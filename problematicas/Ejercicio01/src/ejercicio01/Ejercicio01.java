5aec501b7aa59c933051c6b2facd30de9701795e
tino <sayayinlenin@hotmail.com>
2023-11-27 22:32:58 -0500

    -abimMalware

--- problematicas/Ejercicio01/src/ejercicio01/Ejercicio01.java
+++ problematicas/Ejercicio01/src/ejercicio01/Ejercicio01.java
@@ -21,8 +21,67 @@ public class Ejercicio01 {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
         
-        String nombre_jugador;
-        
+        String cadenaReporte = "";
+        String nombreJugador;
+        String posicionCampo;
+        double estatura;
+        int edad;
+        boolean bandera = true;
+        String salir;
+        int sumaEdades = 0;
+        double sumaEstatura = 0;
+        int contador = 0;
+        double promedioEdad;
+        double promedioEstatura;
+        String edades = "Listado de Edades" ;
+         
+        cadenaReporte = String.format("%s%s\n", cadenaReporte,
+                "Listado de Jugadores");
+
+        do {
+            System.out.println("Ingrese el nombre y apellido del Jugador: ");
+            nombreJugador = entrada.nextLine();
+            System.out.println("Ingrese la posicion en el campo: ");
+            posicionCampo = entrada.nextLine();
+            System.out.println("Ingrese la edad del Jugador: ");
+            edad = entrada.nextInt();
+            entrada.nextLine();
+            System.out.println("Ingrese la estatura: ");
+            estatura = entrada.nextDouble();
+
+            sumaEdades = sumaEdades + edad;
+            sumaEstatura = sumaEstatura + estatura ;
+            contador = contador + 1 ; 
+
+            cadenaReporte = String.format("%s%d.) %s -%s-,"
+                    + " edad %d, estatura %s\n",
+                    cadenaReporte,
+                    contador,
+                    nombreJugador,
+                    posicionCampo,
+                    edad, estatura);
+            
+            edades = String.format("%s\n%s", edades, edad);
+
+            entrada.nextLine();
+            System.out.println("Desea salir del ciclo digite: f");
+            salir = entrada.nextLine();
+            if (salir.equals("f")) {
+                bandera = false;
+            }
+        } while (bandera);
+  
+        promedioEdad = (double) sumaEdades / contador;
+
+        promedioEstatura = (double) sumaEstatura / contador;
+       
+
+        System.out.printf("%s%s\nPromedio de edades: %.2f\n"
+                + "Promedio de estatura: %.2f\n",
+                cadenaReporte,
+                edades,
+                promedioEdad,
+                promedioEstatura);        
 
     }
     
